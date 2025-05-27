d4e86fe359916e6adb8fec7b8e5cfd8ed009c6e7
Koying <koyingchen0523@gmail.com>
2025-05-28 00:39:48 +0800

    更新報名連結

--- src/components/Hero_fb.jsx
+++ src/components/Hero_fb.jsx
@@ -42,7 +42,7 @@ export const Hero = () => {
                   <a
                     className="custom-button-colored w-48 sm:w-36 h-10 mr-0 sm:mr-2 lg:mr-4" href="https://www.facebook.com/groups/300519702549029/" target="_blank"
                   >
-                    6/17 報名開始，請先加入FB社團
+                    5/28 報名開始，請先加入FB社團
                   </a>
                 </div>
               </motion.div>
